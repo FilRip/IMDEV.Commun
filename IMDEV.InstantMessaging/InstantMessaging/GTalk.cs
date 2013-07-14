01bc48fd8172423ccc8b5e41dcba75a1a321ad52
Philippe TREILLE <ptreille@imdeveloppement.com>
2013-07-14 18:46:19 +0200

    Add new library for XMPP instant messaging. Support for connect and send message to GTalk user.

---
+++ IMDEV.InstantMessaging/InstantMessaging/GTalk.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IMDEV.InstantMessaging
+{
+    public class GTalk
+    {
+        private jabber.client.JabberClient _myClient;
+
+        public void connect(string from, string pwd)
+        {
+            _myClient = new jabber.client.JabberClient();
+            _myClient.Server = "gmail.com";
+            _myClient.User = from;
+            _myClient.Password = pwd;
+            _myClient.Connect();
+            _myClient.OnConnect += new jabber.connection.StanzaStreamHandler(_myClient_OnConnect);
+            _myClient.OnAuthError += new jabber.protocol.ProtocolHandler(_myClient_OnAuthError);
+        }
+
+        public void sendMessage(string to, string message)
+        {
+            try
+            {
+                _myClient.OnMessage += new jabber.client.MessageHandler(_myClient_OnMessage);
+                jabber.protocol.client.Message msg = new jabber.protocol.client.Message(_myClient.Document);
+                msg.To = to;
+                msg.Body = message;
+                _myClient.Write(msg);
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Error during sendMessage\r\n" + e.Message);
+            }
+        }
+
+        private void _myClient_OnAuthError(object sender, System.Xml.XmlElement rp)
+        {
+            throw new Exception("Connection refused");
+        }
+
+        public delegate void delegateConnectionOK();
+        public event delegateConnectionOK connectionOK;
+
+        private void _myClient_OnConnect(object sender, jabber.connection.StanzaStream stream)
+        {
+            connectionOK();
+        }
+
+        private void _myClient_OnMessage(object sender, jabber.protocol.client.Message msg)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Close()
+        {
+            _myClient.Close(true);
+        }
+    }
+}
---
+++ IMDEV.InstantMessaging/test_IMDEV.IM/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace test_IMDEV.IM
+{
+    public partial class Form1 : Form
+    {
+        private IMDEV.InstantMessaging.GTalk _myGTalk;
+
+        private delegate void delegateSetConnected();
+        private void setConnected()
+        {
+            this.Text = "Connected";
+        }
+        private void invokeSetConnected()
+        {
+            if (this.InvokeRequired)
+                this.Invoke(new delegateSetConnected(setConnected));
+            else
+                setConnected();
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _myGTalk = new IMDEV.InstantMessaging.GTalk();
+            _myGTalk.connectionOK += new IMDEV.InstantMessaging.GTalk.delegateConnectionOK(_myGTalk_connectionOK);
+            _myGTalk.connect("filrip@gmail.com", "Havealover16384");
+        }
+
+        void _myGTalk_connectionOK()
+        {
+            invokeSetConnected();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            _myGTalk.sendMessage("ptreille@imdeveloppement.com", "Ceci est un test depuis la librairie NET IMDEV");
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (_myGTalk != null)
+                _myGTalk.Close();
+        }
+    }
+}
