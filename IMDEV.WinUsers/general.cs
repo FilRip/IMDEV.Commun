adb7861a7e278f6efc9852de4262f15e78d6bf99
Philippe TREILLE <ptreille@imdeveloppement.com>
2013-11-07 11:48:01 +0100

    Add management of windows users

---
+++ IMDEV.WinUsers/general.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Principal;
+
+namespace IMDEV.WinUsers
+{
+    public class general
+    {
+        public static bool IsUserAdministrator(string username)
+        {
+            try
+            {
+                //get the currently logged in user
+                WindowsIdentity user = new WindowsIdentity(username);
+                WindowsPrincipal principal = new WindowsPrincipal(user);
+                return principal.IsInRole(WindowsBuiltInRole.Administrator);
+            }
+            catch { }
+            return false;
+        }
+
+        public static bool IsUserAdministrator()
+        {
+            return IsUserAdministrator(Environment.UserName);
+        }
+    }
+}
