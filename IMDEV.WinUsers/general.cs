13037dee4625e810b78a970342018fcf6cc70139
Philippe TREILLE <ptreille@imdeveloppement.com>
2013-11-07 12:26:04 +0100

    Add management of user rights in folder

--- IMDEV.WinUsers/general.cs
+++ IMDEV.WinUsers/general.cs
@@ -25,5 +25,32 @@ namespace IMDEV.WinUsers
         {
             return IsUserAdministrator(Environment.UserName);
         }
-    }
+
+        public static bool setRights(string folder, string user, bool removeRights)
+        {
+            return setRights(folder, user, removeRights, false);
+        }
+        public static bool setRights(string folder, string user, bool removeRights, bool write)
+        {
+            System.Security.AccessControl.DirectorySecurity a;
+
+            try
+            {
+                if (System.IO.Directory.Exists(folder))
+                {
+                    a = System.IO.Directory.GetAccessControl(folder);
+                    a.RemoveAccessRuleAll(new System.Security.AccessControl.FileSystemAccessRule(user, System.Security.AccessControl.FileSystemRights.FullControl, System.Security.AccessControl.AccessControlType.Allow));
+                    if (removeRights) return true;
+                    if (write)
+                        a.AddAccessRule(new System.Security.AccessControl.FileSystemAccessRule(user, System.Security.AccessControl.FileSystemRights.FullControl, System.Security.AccessControl.InheritanceFlags.ContainerInherit | System.Security.AccessControl.InheritanceFlags.ObjectInherit, System.Security.AccessControl.PropagationFlags.None, System.Security.AccessControl.AccessControlType.Allow));
+                    else
+                        a.AddAccessRule(new System.Security.AccessControl.FileSystemAccessRule(user, System.Security.AccessControl.FileSystemRights.ReadAndExecute | System.Security.AccessControl.FileSystemRights.ListDirectory | System.Security.AccessControl.FileSystemRights.Read, System.Security.AccessControl.InheritanceFlags.ContainerInherit | System.Security.AccessControl.InheritanceFlags.ObjectInherit, System.Security.AccessControl.PropagationFlags.None, System.Security.AccessControl.AccessControlType.Allow));
+                    System.IO.Directory.SetAccessControl(folder,a);
+                    return true;
+                }
+            }
+            catch { }
+            return false;
+        }
+}
 }
