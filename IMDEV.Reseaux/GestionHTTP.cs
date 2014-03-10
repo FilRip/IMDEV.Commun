b3232283f8d34759dc1d0009c479186c85d26078
Philippe TREILLE <ptreille@imdeveloppement.com>
2014-03-10 20:29:28 +0100

    Classe gestion de communication HTTP

---
+++ IMDEV.Reseaux/GestionHTTP.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Net;
+using System.IO;
+
+namespace IMDEV.Reseaux
+{
+    public class GestionHTTP
+    {
+
+        public string retourneContenu(string adresse)
+        {
+            string retour = "";
+            try
+            {
+                HttpWebRequest oWRequest = (HttpWebRequest)WebRequest.Create(adresse);
+                HttpWebResponse oWResponse = (HttpWebResponse)oWRequest.GetResponse();
+                Stream oS = oWResponse.GetResponseStream();
+                StreamReader oSReader = new StreamReader(oS, System.Text.Encoding.ASCII);
+                retour = oSReader.ReadToEnd();
+                oSReader.Close();
+                oS.Close();
+            }
+            catch { }
+            return retour;
+        }
+
+        public bool envoiContenu(string adresse)
+        {
+            string retour = "";
+            try
+            {
+                HttpWebRequest oWRequest = (HttpWebRequest)WebRequest.Create(adresse);
+                HttpWebResponse oWResponse = (HttpWebResponse)oWRequest.GetResponse();
+                Stream oS = oWResponse.GetResponseStream();
+                StreamReader oSReader = new StreamReader(oS, System.Text.Encoding.ASCII);
+                retour = oSReader.ReadToEnd();
+                oSReader.Close();
+                oS.Close();
+                return true;
+            }
+            catch { }
+            return false;
+        }
+
+    }
+}
